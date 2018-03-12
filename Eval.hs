4e1c0586b60f6524f50ce28e710d97dbee5851f0
Jinxuan Cui <i@cjxol.com>
2018-03-12 16:05:51 +0000

    findVar Maybe

--- Eval.hs
+++ Eval.hs
@@ -8,7 +8,7 @@ type Var = String
 type RelationSymbol = String
 type ConjResult = [[(Var, String)]]
 
-eval :: ExpJudgement -> Judgement
+eval :: Exp -> Judgement
 eval (ExpJudgement vs cq) = judge (evalVarList vs) (evalConjQuer cq)
 
 evalVarList :: VarList -> [Var]
@@ -19,18 +19,26 @@ judge :: [Var] -> ConjResult -> Judgement
 judge _ [] = []
 judge vl (c:cs) = judgeALine vl c : judge vl cs
 
--- judgeALine :: [Var] -> [(Var, String)] -> [String]
+judgeALine :: [Var] -> [(Var, String)] -> [String]
+judgeALine [] _ = []
+judgeALine (v:vs) b = getVar (findVar v b) : judgeALine vs b
 
--- findVar :: Var -> [(Var, String)] -> String
--- findVar _ [] = error "Free variable found"
--- findVar v (l:ls)
-  
+getVar :: Maybe String -> String
+getVar Nothing = error "Free variable found"
+getVar (Just s) = s
+
+findVar :: Var -> [(Var, String)] -> Maybe String
+findVar _ [] = Nothing
+findVar v (l:ls) | v == fst l = Just (snd l)
+                 | otherwise = findVar v ls
 
 -- | Evaluation Conjective Query
 evalConjQuer :: ConjQuer -> ConjResult
 evalConjQuer (ExpRelation r vl) = relation csvData (evalVarList vl)
     where
         csvData = readCsv (r ++ ".csv")
+
+-- evalConjQuer (ExpAnd cq1 cq2) = evalAnd (evalConjQuer cq1) (evalConjQuer cq2)
 
 -- | Evaluation helper functions
 relation :: CsvData -> [Var] -> ConjResult
@@ -41,3 +49,15 @@ relationALine :: [String] -> [Var] -> [(Var, String)]
 relationALine _ [] = []
 relationALine [] (v:vs) = (v, "") : relationALine [] vs
 relationALine (s:ss) (v:vs) = (v, s) : relationALine ss vs
+
+-- evalAnd :: ConjResult -> ConjResult -> ConjResult
+-- evalAnd [] _ = []
+-- evalAnd (cr1:cr1s) cr2s = evalAnd' cr1 cr2s ++ evalAnd cr1s cr2s
+
+-- evalAnd' :: [(Var, String)] -> ConjResult -> ConjResult
+-- evalAnd' _ [] = []
+-- evalAnd' cr1 (cr2:cr2s) = (cr1 ++ cr2) : evalAnd' cr1 cr2s
+
+-- evalAndCheck :: [(Var, String)] -> [(Var, String)] -> Boolean
+-- evalAndCheck [] _ = True
+-- evalAndCheck (b1:b1s) b2s = &&
