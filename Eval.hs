f6aa15d35480f0d1e03e048f75f01b5044b10be2
Yi <zhengyi20201@gmail.com>
2018-03-13 02:49:46 +0000

    add a test case comment

--- Eval.hs
+++ Eval.hs
@@ -63,7 +63,9 @@ evalConjQuer (ExpAnd cq1 cq2) = do
 -- this case seems impossible, cause the ExpEq should always follow some other expressions to make sense. 
 -- evalConjQuer (ExpAnd (ExpEq s1 s2) cq2) = do
 
--- (ExpAnd (ExpAnd (ExpRelation "A" (ExpVarList "x1" (ExpVar "x2"))) (ExpRelation "B" (ExpVarList "x3" (ExpVar "x4")))) (ExpEq "x2" "x3"))
+-- test case:
+-- eval (ExpJudgement (ExpVarList "x1" (ExpVarList "x3" (ExpVarList "x2" (ExpVar "x4")))) (ExpAnd (ExpAnd (ExpRelation "A" (ExpVarList "x1" (ExpVar "x2"))) (ExpRelation "B"
+-- (ExpVarList "x3" (ExpVar "x4")))) (ExpEq "x2" "x3")))
 
 evalEq :: Var -> Var -> [[(String, String)]] -> [[(String, String)]]                                             
 evalEq s1 s2 b = [x| tuple <- b, s1val<-[(findVar s1 tuple)], s1val/=Nothing, s2val<-[(findVar s2 tuple)], s2val/=Nothing, s1val == s2val, x<-[tuple]]
